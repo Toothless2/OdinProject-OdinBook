253c368b17b68d07e0c0d769f11ea070521f208c
Toothless2 <max.rose366@gmail.com>
2020-07-14 20:56:11 +0100

    less messed up migrations

--- db/migrate/20200714154126_change_friend_col_names.rb
+++
@@ -1,6 +0,0 @@
-class ChangeFriendColNames < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :friends, 'user1_id', :sender
-    rename_column :friends, 'user2_id', :recipient
-  end
-end
--- db/migrate/20200714160218_drop_table_likes.rb
+++ db/migrate/20200714160218_drop_table_likes.rb
@@ -1,8 +1,5 @@
 class DropTableLikes < ActiveRecord::Migration[5.2]
   def change
-    drop_table :friends
-
-
     create_table :friends do |t|
       t.integer :sender
       t.integer :recipient
